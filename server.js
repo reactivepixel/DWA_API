cf4a9fd5dba369baf94dcc73884fe689206735c2
Chris Chapman <Chapman@apextion.com>
2017-07-12 18:21:27 -0400

    removed erronious line

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use('/', require('./src/routes')(express));
 app.set('port', process.env.PORT || 3000);
-// const port = 3000;
 
 exports.instance = app.listen(app.get('port'), () => {
 	console.log(`Server running on port ${app.get('port')}`);
